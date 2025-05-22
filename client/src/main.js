456e0cf7742e7942b7888545bd95d9709ce7746c
lukeplechaty <lukeplechaty@hotmail.co.uk>
2025-05-22 15:34:07 +0100

    fix: url replaced

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000";
+const url = "https://guestbook-server-4ymb.onrender.com";
 const form = document.getElementById("form-message");
 
 form.addEventListener("submit", (event) => {
